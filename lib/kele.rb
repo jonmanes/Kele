5c43b99aa8a7362ef128c247ee6f8d77aa4b9ab7
jon manes <jon.manes@gmail.com>
2016-12-13 20:57:47 -0600

    Added get_me to aquire my info in a hash

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class Kele
 
@@ -7,15 +8,12 @@ class Kele
   base_uri "https://www.bloc.io/api/v1"
 
   def initialize(email, password)
-    options = {
-      body: {
-        email: email,
-        password: password
-      }
-    }
-
-    @auth_token = self.class.post('/sessions', options)['auth_token']
+    @auth_token = self.class.post('/sessions', body: { email: email, password: password })['auth_token']
     raise InvalidError unless @auth_token
+  end
+
+  def get_me()
+    @user_data = JSON.parse(self.class.get('/users/me', headers: { "authorization" => @auth_token }).body)
   end
 
 end
